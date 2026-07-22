e272f57d34f808872dadfef307ce867698721c8c
Md Antor Mia <mdantormia1779@gmail.com>
2026-07-23 04:14:11 +0600

    done

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
-import { PrismaClient } from "../../generated/prisma/client"; // Apnar folder structure onujayi relative path
+import { PrismaClient } from "../../generated/prisma/client";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
